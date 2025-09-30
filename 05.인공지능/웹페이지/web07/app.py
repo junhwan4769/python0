79e633758937b45b192fdc8c4811f955c9963df6
junhwan4769 <bjl4769@naver.com>
2025-09-30 18:22:08 +0900

    3242

--- 05.인공지능/웹페이지/web07/app.py
+++ 05.인공지능/웹페이지/web07/app.py
@@ -4,6 +4,25 @@ import pandas as pd
 app = Flask(__name__, template_folder='temp', static_folder='static')
 
 
+@app.route('/predict')
+def predict():
+    from sklearn.neighbors import KNeighborsClassifier
+    model = KNeighborsClassifier()
+
+    test = pd.read_csv('data/타이타닉/테스트.csv')
+    train = pd.read_csv('data/타이타닉/훈련.csv')
+    X = train.iloc[:, 1:].values
+    y = train.iloc[:, 0].values
+    model.fit(X, y)
+
+    X_pred = test.iloc[:, :].values
+    y_pred = model.predict(X_pred)
+
+    df_test = pd.read_csv('data/타이타닉/test.csv')
+    df_test['pred'] = y_pred
+    df_test.to_csv('data/타이타닉/test.csv', index=False)
+    return 'success'
+
 @app.route('/data')
 def data():
     page = int(request.args['page'])
@@ -11,9 +30,6 @@ def data():
     start = (page-1) * size #0, 5, 10
     end = (page*size) #4, 9, 14
     df = pd.read_csv('data/타이타닉/test.csv')
-    cols = ['Name', 'Sex', 'Age', 'Fare', 'Pclass', 'Embarked']
-    df = df[cols]
-    df.columns = ['성명', '성별', '나이', '요금', '등석', '항구']
     total = len(df)
     df = df[start:end]
     table = df.to_html(classes='table table-striped table-dark table-hover', index=True)
